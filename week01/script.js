c2da4e5668fd056c45273aecfdc5ce647d5a6102
BYU-WDD130 <henriquezdery@gmail.com>
2025-07-05 17:42:25 -0400

    index

--- week01/script.js
+++ week01/script.js
@@ -1,3 +1,7 @@
 document.getElementById("currentyear").textContent = new Date().getFullYear();
 
 document.getElementById("LastModified").textContent = "Last Modified: " + document.lastModified;
+
+
+
+let nLastModif = Date.parse(document.lastModified);
