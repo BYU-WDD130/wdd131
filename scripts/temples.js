82c45b8024650900702f3bd121e018f0b4f3dea9
BYU-WDD130 <henriquezdery@gmail.com>
2025-07-10 20:40:32 -0400

    temples

--- scripts/temples.js
+++ scripts/temples.js
@@ -3,7 +3,7 @@ document.getElementById("currentyear").textContent = new Date().getFullYear();
 document.getElementById("LastModified").textContent = "Last Modified: " + document.lastModified;
 
 const hamburger = document.getElementById("hamburger");
-const navMenu = document.querySelector("nav ul");
+const navMenu = document.querySelector("nav a");
 
 hamburger.addEventListener("click", () => {
     navMenu.classList.toggle("open");
