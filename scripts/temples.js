287dea12c304dcbc3545265d79ed4f6df527e042
BYU-WDD130 <henriquezdery@gmail.com>
2025-07-21 15:41:22 -0400

    temples

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,3 +1,47 @@
+document.addEventListener("DOMContentLoaded", function () { 
+
+  const openMenuBtn = document.getElementById("open-menu"); 
+
+  const closeMenuBtn = document.getElementById("close-menu"); 
+
+  const mobileMenu = document.getElementById("mobile-menu"); 
+
+  
+
+  openMenuBtn.addEventListener("click", () => { 
+
+    mobileMenu.classList.add("active"); 
+
+  }); 
+
+  
+
+  closeMenuBtn.addEventListener("click", () => { 
+
+    mobileMenu.classList.remove("active"); 
+
+  }); 
+
+  
+
+  // Opcional: cerrar el menú al hacer clic en un enlace 
+
+  const navLinks = mobileMenu.querySelectorAll("a"); 
+
+  navLinks.forEach(link => 
+
+    link.addEventListener("click", () => { 
+
+      mobileMenu.classList.remove("active"); 
+
+    }) 
+
+  ); 
+
+}); 
+
+
+/*
 document.getElementById("currentyear").textContent = new Date().getFullYear();
 
 document.getElementById("LastModified").textContent = "Last Modified: " + document.lastModified;
@@ -13,3 +57,5 @@ menuButton.addEventListener("click", () => {
     const isOpen = navMenu.classList.contains("show");
     menuButton.textContent = isOpen ? "❌" : "☰";
 });
+
+*/
