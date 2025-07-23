e4cc31750c0bdecd389acee8d8a51bb0a71f9984
BYU-WDD130 <henriquezdery@gmail.com>
2025-07-23 15:59:54 -0400

    temples

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,11 +1,11 @@
-
+//actual year
 document.getElementById("currentyear").textContent = new Date().getFullYear();
-
+//last modification
 document.getElementById("LastModified").textContent = "Last Modified: " + document.lastModified;
 
 
 document.addEventListener("DOMContentLoaded", function () { 
-
+//elementos del menu movil
   const openMenuBtn = document.getElementById("open-menu"); 
 
   const closeMenuBtn = document.getElementById("close-menu"); 
@@ -13,59 +13,39 @@ document.addEventListener("DOMContentLoaded", function () {
   const mobileMenu = document.getElementById("mobile-menu"); 
 
   
-
+//abrir menu
   openMenuBtn.addEventListener("click", () => { 
 
     mobileMenu.classList.add("active"); 
+  });
 
-  }); 
+//cerrar menu
+  closeMenuBtn.addEventListener("click", () => { 
+    mobileMenu.classList.remove("active"); 
+     });
+
+//menu hamburgueza (para navegacion principal)
+
+  const hamButton = document.querySelector('#menu');
+  const navigation = document.querySelector('.navigation');
+  const title = document.querySelector('#title');
+  const header = document.querySelector("header");
 
   hamButton.addEventListener('click', () => {
-    navigation.classList.toggle('open');
-    hamButton.classList.toggle('open');
-    header.classList.toggle("open");
-    title.classList.toggle('hide');
+   navigation.classList.toggle('open');
+   hamButton.classList.toggle('open');
+   header.classList.toggle("open");
+   title.classList.toggle('hide');
+  });
+});
 
-  closeMenuBtn.addEventListener("click", () => { 
-
-    mobileMenu.classList.remove("active"); 
-
-  }); 
 
   
 
-  // Opcional: cerrar el menú al hacer clic en un enlace 
-
-  const navLinks = mobileMenu.querySelectorAll("a"); 
-
-  navLinks.forEach(link => 
-
-    link.addEventListener("click", () => { 
-
-      mobileMenu.classList.remove("active"); 
-
-    }) 
-
-  ); 
-
-}); 
-
-
-/*
-document.getElementById("currentyear").textContent = new Date().getFullYear();
-
-document.getElementById("LastModified").textContent = "Last Modified: " + document.lastModified;
 
 
 
-const menuButton = document.getElementById("menuBtn");
-const navMenu = document.getElementById("navMenu");
+ 
 
-menuButton.addEventListener("click", () => {
-    navMenu.classList.toggle("show");
 
-    const isOpen = navMenu.classList.contains("show");
-    menuButton.textContent = isOpen ? "❌" : "☰";
-});
 
-*/
