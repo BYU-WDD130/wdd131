edf4f032aabe0231a58a8e44efc9dc69ea848e31
BYU-WDD130 <henriquezdery@gmail.com>
2025-07-24 13:50:55 -0400

    temples

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -64,3 +64,31 @@ const temples = [
 document.getElementById("currentyear").textContent = new Date().getFullYear();
 //last modification
 document.getElementById("LastModified").textContent = "Last Modified: " + document.lastModified;
+
+const menuButton = document.getElementById("menu");
+const mobileMenu = document.getElementById("mobileMenu");
+
+menuButton.addEventListener("click", () => {
+  mobileMenu.classList.toggle("active");
+  
+
+  if (mobileMenu.classList.contains("active")) {
+    menuButton.textContent = "✖";
+  } else {
+    menuButton.textContent = "☰";
+  }
+});
+
+//menu hamburgueza (para navegacion principal)
+
+  const hamButton = document.querySelector('#menu');
+  const navigation = document.querySelector('.navigation');
+  const title = document.querySelector('#title');
+  const header = document.querySelector("header");
+
+  hamButton.addEventListener('click', () => {
+   navigation.classList.toggle('open');
+   hamButton.classList.toggle('open');
+   header.classList.toggle("open");
+   title.classList.toggle('hide');
+  });
