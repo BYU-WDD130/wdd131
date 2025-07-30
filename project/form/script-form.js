bb2cd0820e449b2f6e588ec400a52b4d6d417ab0
BYU-WDD130 <henriquezdery@gmail.com>
2025-07-30 16:00:22 -0400

    form

---
+++ project/form/script-form.js
@@ -0,0 +1,16 @@
+// Product data array
+const products = [
+  { id: "cake", name: "3D Gelatin Cake" },
+  { id: "cupcake", name: "Cupcake Box" },
+  { id: "mousse", name: "Chocolate Mousse" },
+  { id: "seasonal", name: "Seasonal Treat" }
+];
+
+// Populate select dropdown
+const productSelect = document.getElementById("product");
+products.forEach(product => {
+  const option = document.createElement("option");
+  option.value = product.id;
+  option.textContent = product.name;
+  productSelect.appendChild(option);
+});
