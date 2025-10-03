515a78445165b6b20075d46046a2dd48d9430011
BYU-WDD130 <henriquezdery@gmail.com>
2025-10-02 20:35:11 -0400

    pruevas

--- CSE111/week4/chemistry.py
+++ CSE111/week4/chemistry.py
@@ -1,110 +1,100 @@
+from formula import parse_formula   # provided function
+from collections import defaultdict
+
+# Periodic Table (symbol → [name, atomic_mass, atomic_number])
 def make_periodic_table():
-    periodic_table_dict = {
-    # symbol: [name, atomic_mass]
-        "Ac": ["Actinium", 227],
-        "Ag": ["Silver", 107.8682],
-        "Al": ["Aluminum", 26.9815386],
-        "Ar": ["Argon", 39.948],
-        "As": ["Arsenic", 74.9216],
-        "At": ["Astatine", 210],
-        "Au": ["Gold", 196.966569],
-        "B": ["Boron", 10.811],
-        "Ba": ["Barium", 137.327],
-        "Be": ["Beryllium", 9.012182],
-        "Bi": ["Bismuth", 208.9804],
-        "Br": ["Bromine", 79.904],
-        "C": ["Carbon", 12.0107],
-        "Ca": ["Calcium", 40.078],
-        "Cd": ["Cadmium", 112.411],
-        "Ce": ["Cerium", 140.116],
-        "Cl": ["Chlorine", 35.453],
-        "Co": ["Cobalt", 58.933195],
-        "Cr": ["Chromium", 51.9961],
-        "Cs": ["Cesium", 132.9054519],
-        "Cu": ["Copper", 63.546],
-        "Dy": ["Dysprosium", 162.5],
-        "Er": ["Erbium", 167.259],
-        "Eu": ["Europium", 151.964],
-        "F": ["Fluorine", 18.9984032],
-        "Fe": ["Iron", 55.845],
-        "Fr": ["Francium", 223],
-        "Ga": ["Gallium", 69.723],
-        "Gd": ["Gadolinium", 157.25],
-        "Ge": ["Germanium", 72.64],
-        "H": ["Hydrogen", 1.00794],
-        "He": ["Helium", 4.002602],
-        "Hf": ["Hafnium", 178.49],
-        "Hg": ["Mercury", 200.59],
-        "Ho": ["Holmium", 164.93032],
-        "I": ["Iodine", 126.90447],
-        "In": ["Indium", 114.818],
-        "Ir": ["Iridium", 192.217],
-        "K": ["Potassium", 39.0983],
-        "Kr": ["Krypton", 83.798],
-        "La": ["Lanthanum", 138.90547],
-        "Li": ["Lithium", 6.941],
-        "Lu": ["Lutetium", 174.9668],
-        "Mg": ["Magnesium", 24.305],
-        "Mn": ["Manganese", 54.938045],
-        "Mo": ["Molybdenum", 95.96],
-        "N": ["Nitrogen", 14.0067],
-        "Na": ["Sodium", 22.98976928],
-        "Nb": ["Niobium", 92.90638],
-        "Nd": ["Neodymium", 144.242],
-        "Ne": ["Neon", 20.1797],
-        "Ni": ["Nickel", 58.6934],
-        "Np": ["Neptunium", 237],
-        "O": ["Oxygen", 15.9994],
-        "Os": ["Osmium", 190.23],
-        "P": ["Phosphorus", 30.973762],
-        "Pa": ["Protactinium", 231.03588],
-        "Pb": ["Lead", 207.2],
-        "Pd": ["Palladium", 106.42],
-        "Pm": ["Promethium", 145],
-        "Po": ["Polonium", 209],
-        "Pr": ["Praseodymium", 140.90765],
-        "Pt": ["Platinum", 195.084],
-        "Pu": ["Plutonium", 244],
-        "Ra": ["Radium", 226],
-        "Rb": ["Rubidium", 85.4678],
-        "Re": ["Rhenium", 186.207],
-        "Rh": ["Rhodium", 102.9055],
-        "Rn": ["Radon", 222],
-        "Ru": ["Ruthenium", 101.07],
-        "S": ["Sulfur", 32.065],
-        "Sb": ["Antimony", 121.76],
-        "Sc": ["Scandium", 44.955912],
-        "Se": ["Selenium", 78.96],
-        "Si": ["Silicon", 28.0855],
-        "Sm": ["Samarium", 150.36],
-        "Sn": ["Tin", 118.71],
-        "Sr": ["Strontium", 87.62],
-        "Ta": ["Tantalum", 180.94788],
-        "Tb": ["Terbium", 158.92535],
-        "Tc": ["Technetium", 98],
-        "Te": ["Tellurium", 127.6],
-        "Th": ["Thorium", 232.03806],
-        "Ti": ["Titanium", 47.867],
-        "Tl": ["Thallium", 204.3833],
-        "Tm": ["Thulium", 168.93421],
-        "U": ["Uranium", 238.02891],
-        "V": ["Vanadium", 50.9415],
-        "W": ["Tungsten", 183.84],
-        "Xe": ["Xenon", 131.293],
-        "Y": ["Yttrium", 88.90585],
-        "Yb": ["Ytterbium", 173.04],
-        "Zn": ["Zinc", 65.38],
-        "Zr": ["Zirconium", 91.224]
+    table = {
+        "H": ["Hydrogen", 1.00794, 1],
+        "He": ["Helium", 4.002602, 2],
+        "Li": ["Lithium", 6.941, 3],
+        "Be": ["Beryllium", 9.012182, 4],
+        "B": ["Boron", 10.811, 5],
+        "C": ["Carbon", 12.0107, 6],
+        "N": ["Nitrogen", 14.0067, 7],
+        "O": ["Oxygen", 15.9994, 8],
+        "F": ["Fluorine", 18.9984032, 9],
+        "Na": ["Sodium", 22.98976928, 11],
+        "Mg": ["Magnesium", 24.305, 12],
+        "Al": ["Aluminum", 26.9815386, 13],
+        "Si": ["Silicon", 28.0855, 14],
+        "P": ["Phosphorus", 30.973762, 15],
+        "S": ["Sulfur", 32.065, 16],
+        "Cl": ["Chlorine", 35.453, 17],
+        "K": ["Potassium", 39.0983, 19],
+        "Ca": ["Calcium", 40.078, 20],
+        "Fe": ["Iron", 55.845, 26],
+        "Cu": ["Copper", 63.546, 29],
+        "Zn": ["Zinc", 65.38, 30],
+        "Ag": ["Silver", 107.8682, 47],
+        "I": ["Iodine", 126.90447, 53],
+        "Au": ["Gold", 196.966569, 79],
+        "Hg": ["Mercury", 200.59, 80],
+        "Pb": ["Lead", 207.2, 82]
+        # Add more as needed
     }
-    return periodic_table_dict
+    return table
 
-# Main function
+
+def compute_molar_mass(symbol_quantity_list, periodic_table):
+    """Compute total molar mass for a molecule."""
+    total_mass = 0.0
+    for symbol, qty in symbol_quantity_list:
+        if symbol not in periodic_table:
+            raise ValueError(f"Unknown element: {symbol}")
+        atomic_mass = periodic_table[symbol][1]
+        total_mass += atomic_mass * qty
+    return total_mass
+
+
+def compute_protons(symbol_quantity_list, periodic_table):
+    """Compute total number of protons in the molecule."""
+    total_protons = 0
+    for symbol, qty in symbol_quantity_list:
+        atomic_number = periodic_table[symbol][2]
+        total_protons += atomic_number * qty
+    return total_protons
+
+
 def main():
-    periodic_table = make_periodic_table()
-    print("Periodic Table:")
-    for symbol, info in periodic_table.items():
-        print(symbol, ":", info)
+    # Known formulas dictionary (for extra credit)
+    known_formulas = {
+        "H2O": "Water",
+        "CO2": "Carbon Dioxide",
+        "O2": "Oxygen Gas",
+        "NaCl": "Table Salt",
+        "C6H12O6": "Glucose"
+    }
 
-# This allows the script to be run directly
+    # Get user input
+    formula = input("Enter the chemical formula of the sample: ")
+    mass = float(input("Enter the mass of the sample in grams: "))
+
+    # Build periodic table
+    periodic_table = make_periodic_table()
+
+    # Parse formula into element counts
+    symbol_quantity_list = parse_formula(formula, periodic_table)
+
+    # Compute molar mass
+    molar_mass = compute_molar_mass(symbol_quantity_list, periodic_table)
+
+    # Compute number of moles
+    moles = mass / molar_mass
+
+    # Print results
+    print(f"\nResults for {formula}:")
+    print(f"Molar mass: {molar_mass:.5f} g/mol")
+    print(f"Number of moles in {mass} g: {moles:.5f} mol")
+
+    # Extra: check if formula is in known list
+    if formula in known_formulas:
+        print(f"This compound is commonly known as: {known_formulas[formula]}")
+
+    # Extra: compute number of protons
+    total_protons = compute_protons(symbol_quantity_list, periodic_table)
+    print(f"Total number of protons in one molecule: {total_protons}")
+
+
+# Run only if executed directly
 if __name__ == "__main__":
     main()
