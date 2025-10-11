72ccbd839971c1667506e10daa00e34b042b84df
BYU-WDD130 <henriquezdery@gmail.com>
2025-10-11 18:59:44 -0400

    reposteria

--- pruevas/prueva.js
+++ pruevas/prueva.js
@@ -10,3 +10,43 @@ abrir.addEventListener("click", () => {
 cerrar.addEventListener("click", () => {
     nav.classList.remove("visible");
 })
+
+  const carousel = document.querySelector('.carousel-images');
+  const images = document.querySelectorAll('.carousel-images img');
+  const prevBtn = document.querySelector('.prev');
+  const nextBtn = document.querySelector('.next');
+
+  let index = 0;
+  const total = images.length;
+  let autoSlide = setInterval(nextImage, 3000); // cambia cada 3 segundos
+
+  function showImage() {
+    carousel.style.transform = `translateX(${-index * 100}%)`;
+  }
+
+  function nextImage() {
+    index = (index + 1) % total;
+    showImage();
+  }
+
+  function prevImage() {
+    index = (index - 1 + total) % total;
+    showImage();
+  }
+
+  // Botones
+  nextBtn.addEventListener('click', () => {
+    nextImage();
+    resetAutoSlide();
+  });
+
+  prevBtn.addEventListener('click', () => {
+    prevImage();
+    resetAutoSlide();
+  });
+
+  // Reinicia el temporizador si el usuario usa los botones
+  function resetAutoSlide() {
+    clearInterval(autoSlide);
+    autoSlide = setInterval(nextImage, 3000);
+  }
